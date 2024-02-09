6770cad9c93bd9397a7460ceb0a94045f12db8fc
Eronar Alves <alveseronar@gmail.com>
2024-02-08 21:13:29 -0300

    feat: 🎸 scaffolding

---
+++ src/styles/global.ts
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+
+
+export const GlogalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    width: 100%;
+    height: 100%;
+    font-family: 'Montserrat', sans-serif;
+  }
+`;  
